a08f53f779006fac20c3f71b58c7a07111a73258
Matth5050 <matthew.f.herbert@gmail.com>
2022-06-13 11:39:21 -0700

    added new functionality to allow user to click and see details of the location
    Co-authored-by: Winnie Wang <weijwang18@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,16 +1,3 @@
-let hawaii = {
-  location: 'america',
-  landmarks: 'beach',
-  time: 'summer',
-  notes: 'fun'
-};
-
-let shanghai = {
-  location: "china",
-  landmarks: "Pearl TV Tower",
-  time: "summer",
-  notes: "home"
-}
 
 function Place(location, landmarks, time, notes) {
   this.location = location;
@@ -20,4 +7,62 @@ function Place(location, landmarks, time, notes) {
 };
 
 
-let leavenworth = new Place("leavenworth","museum","summer","Reindeers are cute!")
+let places = [
+  { name: "hawaii",
+    location: 'america',
+    landmarks: 'beach',
+    time: 'summer',
+    notes: 'fun'
+   },
+   { name: "shanghai",
+     location: "china",
+     landmarks: "Pearl TV Tower",
+     time: "summer",
+     notes: "home"
+  },
+  { name:"leavenworth",
+    location: "america",
+    landmarks: "museum",
+    time: "summer",
+    notes: "Reindeers are cute!" 
+  },
+  { name: "Texas",
+    location: "america",
+    landmarks: "whatever",
+    time: "summer",
+    notes: "fun"
+  }
+]
+
+// Business logic
+$(document).ready(function(){
+  $("p.hawaii").click(function(event){
+    event.preventDefault();
+      $("ul.hawaii").append("<li>" + places[0].location + "</li>");
+      $("ul.hawaii").append("<li>" + places[0].landmarks + "</li>");
+      $("ul.hawaii").append("<li>" + places[0].time + "</li>");
+      $("ul.hawaii").append("<li>" + places[0].notes + "</li>");
+    })
+  $("p.shanghai").click(function(event){
+    event.preventDefault();
+      $("ul.shanghai").append("<li>" + places[1].location + "</li>");
+      $("ul.shanghai").append("<li>" + places[1].landmarks + "</li>");
+      $("ul.shanghai").append("<li>" + places[1].time + "</li>");
+      $("ul.shanghai").append("<li>" + places[1].notes + "</li>");
+    })
+  $("p.leavenworth").click(function(event){
+    event.preventDefault();
+      $("ul.leavenworth").append("<li>" + places[2].location + "</li>");
+      $("ul.leavenworth").append("<li>" + places[2].landmarks + "</li>");
+      $("ul.leavenworth").append("<li>" + places[2].time + "</li>");
+      $("ul.leavenworth").append("<li>" + places[2].notes + "</li>");
+    })
+  $("p.texas").click(function(event){
+    event.preventDefault();
+      $("ul.texas").append("<li>" + places[3].location + "</li>");
+      $("ul.texas").append("<li>" + places[3].landmarks + "</li>");
+      $("ul.texas").append("<li>" + places[3].time + "</li>");
+      $("ul.texas").append("<li>" + places[3].notes + "</li>");
+    })
+  });
+ 
